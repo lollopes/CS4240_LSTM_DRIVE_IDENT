9c54ca86b887a08ef3f764194674d9d28725d55d
lorenzo <lollopes1995@gmail.com>
2022-06-11 16:09:25 +0200

    fixed custom dataloader

--- main4_gpu.py
+++ main4_gpu.py
@@ -52,70 +52,98 @@ class DatasetDrivers(Dataset):
                                                                  'Torque_scaling_factor(standardization)',
                                                                  'Glow_plug_control_request'], window_size=16, normalize=True, normalize_method='mean_std'):
         print("Loading")
+        
         self._window_size=window_size
         self._data=pd.read_csv(file_path)
-        self._data.sort_values(by="Class", inplace=True)
+        
+        # The data is sorted by Class A,B,C the indexes of the dataframe have restarted by ignore index
+        self._data = self._data.sort_values(by=['Class'], inplace=False,ignore_index = True) 
         print("Removing extra columns, initial data size is:")
         print(len(self._data))
-        n_per_class=[]
+        
+        
+        # class_uniq contains the letters of the drivers A,B and it loops across all of them 
         for class_uniq in list(self._data['Class'].unique()):
-            tot_number=sum(self._data['Class']==class_uniq)
-            n_per_class.append(tot_number-tot_number%window_size)
+            # find the total number of elements belonging to a class
+            tot_number=sum(self._data['Class']==class_uniq) 
+            #number of elements to drop so that the class element is divisible by window size 
             to_drop=tot_number%window_size
+            #returns the index of the first element of the class
             index_to_start_removing=self._data[self._data['Class']==class_uniq].index[0]
-            #import pdb; pdb.set_trace()
+            #drop element from first element to the element required 
             self._data.drop(self._data.index[index_to_start_removing:index_to_start_removing+to_drop],inplace=True)
+            
+           
         print("After removing, the size becomes:")
         print(len(self._data))
-        self._data.reset_index(inplace=True)
-        #import pdb; pdb.set_trace()
-
-        index_starting_class=[]
-        #index_starting_class.append(0)
+        
+        #resetting index of dataframe after dropping values 
+        self._data = self._data.reset_index()
+        self._data = self._data.drop(['index'], axis=1)
+        
+        index_starting_class=[] # This array contains the starting index of each class in the df
         for class_uniq in list(self._data['Class'].unique()):
+            #Appending the index of first element of each clas
             index_starting_class.append(self._data[self._data['Class']==class_uniq].index[0])
-        #import pdb; pdb.set_trace()
-        index_starting_class.append(len(self._data))
+        
+        # Create the sequence of indexs of the windows 
         sequences=[]
         for i in range(len(index_starting_class)):
-            if i!=len(index_starting_class)-1:#index_starting_class[-1]:
-                ranges=np.arange(index_starting_class[i], index_starting_class[i+1])
-            
+            #check if beginning of next class is there 
+            if i!=len(index_starting_class)-1:
+                ranges=np.arange(index_starting_class[i], index_starting_class[i+1])       
             for j in range(0,len(ranges),int(self._window_size/2)):
                 if len(ranges[j:j+self._window_size])==16:
                     sequences.append(ranges[j:j+self._window_size])
         self._sequences=sequences
         
+        # Dropping columns which have constant measurements because they would return nan in std
         print("Drop columns: ")
         print(classes_to_drop)
-        self._labels=self._data['Class'].values
+        
+        #take only the 'Class' which are the actual labels and store it in the labels of self 
+        self._labels=self._data['Class']
         self._data.drop(classes_to_drop, inplace=True, axis=1)
+        
+        # Function to normalize the data either with min_max or mean_std
         if normalize:
             for col in self._data.columns:
                 if normalize_method=='min_max':
                     self._data[col]=(self._data[col]-self._data[col].min())/(self._data[col].max()-self._data[col].min())
                 elif normalize_method=="mean_std":
                     self._data[col]=(self._data[col]-self._data[col].mean())/(self._data[col].std())
+                    
+        # Create the array holding the windowed multidimensional arrays 
         X=np.empty((len(sequences), self._window_size, len(self._data.columns)))
-        y=[]#np.empty((len(sequences), ))
+        y=[]
+        
         for n_row, sequence in enumerate(sequences):
             X[n_row,:,:]=self._data.iloc[sequence]
-            y.append(self._labels[sequence[0]])
+            # I am saying that the corresponding driver of the sequence is the driver at first sequence
+            # This should be OK but not sure, maybe take the most recurrent driver in the sequence ? 
+            y.append(self._labels[sequence[0]]) 
+            #y.append(self._labels.iloc[sequence])
+       
+
         assert len(y)==len(X)
-        self._X=X
+        #Assing the windowed dataset to the X of self 
+        self._X= X
+        
+        # targets is a transformed version of y with drivers are encoded into 0 to 9 
         targets = preprocessing.LabelEncoder().fit_transform(y)
-        targets = torch.as_tensor(targets)
-        #targets_tensor = torch.nn.functional.one_hot(targets.to(torch.int64), num_classes=10)
-        self._y=targets
-    
+        targets = torch.as_tensor(targets) # just converting it to a pytorch tensor 
+        self._y=targets # assign it to y of self 
+        
+        #import pdb; pdb.set_trace()
+
+        
     def __len__(self):
         return len(self._X)
+        
     
     def __getitem__(self, index):
         return torch.FloatTensor(self._X[index,:,:]), self._y[index]
 
-
-# Istance of the dataset 
 a=DatasetDrivers()
 
 
